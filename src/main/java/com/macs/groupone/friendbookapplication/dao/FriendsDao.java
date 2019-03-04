47ea8d87e45048afb08c4ebbb00d081d0c604f76
Yash <yashdesai,10795@gmail.com>
2019-03-03 22:10:27 -0400

    Create FriendsDao java

---
+++ src/main/java/com/macs/groupone/friendbookapplication/dao/FriendsDao.java
@@ -0,0 +1,14 @@
+package com.macs.groupone.friendbookapplication.dao;
+
+import java.util.Collection;
+
+import com.macs.groupone.friendbookapplication.model.User;
+
+public interface FriendsDao {
+	
+	long addFriend(User user, User friend);
+    void removeFriend(User user, User friend);
+    long getNumberOfFriends(User user, String searchText);
+    Collection<User> getFriendList(User user, int recordsPerPage, int i, String searchText);
+
+}
