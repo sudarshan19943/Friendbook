a1e27a8fc9f7513792264c14ba7a43372cd4eaa0
suman_saket <smn.singh666@gmail.com>
2019-03-03 21:58:09 -0400

    adding Profile Controller Service

--- src/main/java/com/macs/groupone/friendbookapplication/controller/ProfileController.java
+++ src/main/java/com/macs/groupone/friendbookapplication/controller/ProfileController.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.macs.groupone.friendbookapplication.model.User;
+import com.macs.groupone.friendbookapplication.service.CountryAndStateService;
 import com.macs.groupone.friendbookapplication.service.UserService;
 
 @Controller
@@ -32,8 +33,8 @@ public class ProfileController {
 	@Autowired
 	CountryAndStateService countryAndStateService;
 	
-	@Autowired
-	AvatarService avatarService;
+	/*@Autowired
+	AvatarService avatarService;*/
 
 	// show login page
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
@@ -51,7 +52,7 @@ public class ProfileController {
 		modelAndView.addObject("Countries",countryList);
 		
 		//initially default avatar
-		avatarService.getDeafultProfileAvatar(userByEmail.getId());
+		//avatarService.getDeafultProfileAvatar(userByEmail.getId());
 		
 		modelAndView.setViewName("profile");
 	    return modelAndView;
