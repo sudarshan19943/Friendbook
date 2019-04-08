5002676e67099557c2990451878f26eab74b6a44
Sudarshan Suresh Srikant <sudarshan1994@gmail.com>
2019-04-08 11:37:08 -0300

    Test cases for friends feature

--- src/test/java/com/macs/groupone/friendbookapplication/service/FriendsServiceTest.java
+++ src/test/java/com/macs/groupone/friendbookapplication/service/FriendsServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -59,6 +60,30 @@ public class FriendsServiceTest {
 	}
 	
 	@Test
+	public void testUserIDremoveUserFromFriendsList()
+	{
+		User user1 = factory.makeMockUserObject();
+		User user2 = factory.makeMockUserObject();
+		User user3 = factory.makeMockUserObject();
+		User user4 = factory.makeMockUserObject();
+		
+		user1.setId(0);
+		user2.setId(1);
+		user3.setId(2);
+		user4.setId(3);
+		
+		ArrayList<User> friendList = new ArrayList<>();
+		
+		friendList.add(user1);
+		friendList.add(user2);
+		friendList.add(user3);
+		
+		friendsService.removeUserFromFriendsList(user4, friendList);
+		assertEquals(3, friendList.size());
+		
+	}
+	
+	@Test
 	public void testForIndexOutOfBoundsRemoveUserFromFriendsList()
 	{
 		User user1 = factory.makeMockUserObject();
@@ -92,4 +117,101 @@ public class FriendsServiceTest {
 		friendList.get(0);
 	}
 	
+	@Test
+	public void testremoveFriendsFromUserListSize()
+	{
+		User user1 = factory.makeMockUserObject();
+		User user2 = factory.makeMockUserObject();
+		User user3 = factory.makeMockUserObject();
+		User user4 = factory.makeMockUserObject();
+		
+		user1.setId(0);
+		user2.setId(1);
+		user3.setId(2);
+		user4.setId(3);
+		
+		ArrayList<User> userList = new ArrayList<>();
+		ArrayList<User> friendList = new ArrayList<>();
+		
+		userList.add(user1);
+		userList.add(user2);
+		userList.add(user3);
+		friendList.add(user2);
+		
+		friendsService.removeFriendsfromUserList(userList, friendList);
+		assertEquals(2, userList.size());
+		
+	}
+	
+	@Test
+	public void testUserIDRemoveFriendsFromUserList()
+	{
+		User user1 = factory.makeMockUserObject();
+		User user2 = factory.makeMockUserObject();
+		User user3 = factory.makeMockUserObject();
+		User user4 = factory.makeMockUserObject();
+		
+		user1.setId(0);
+		user2.setId(1);
+		user3.setId(2);
+		user4.setId(3);
+		
+		ArrayList<User> userList = new ArrayList<>();
+		ArrayList<User> friendList = new ArrayList<>();
+		
+		userList.add(user1);
+		userList.add(user2);
+		friendList.add(user4);
+		
+		friendsService.removeFriendsfromUserList(userList, friendList);
+		assertEquals(2, userList.size());
+		
+	}
+	
+	@Test
+	public void testForIndexOutOfBoundsRemoveFriendsFromUserList()
+	{
+		User user1 = factory.makeMockUserObject();
+		user1.setId(0);
+		exception.expect(IndexOutOfBoundsException.class);
+		ArrayList<User> friendList = new ArrayList<>();
+		ArrayList<User> userList = new ArrayList<>();
+		userList.add(user1);
+		friendList.add(user1);
+		friendsService.removeFriendsfromUserList(userList, friendList);
+		friendList.get(2);
+		
+	}
+	
+	@Test
+	public void testForArrayOutOfBoundsExceptionRemoveFriendsFromUserList()
+	{
+		User user1 = factory.makeMockUserObject();
+		user1.setId(0);		
+		exception.expect(ArrayIndexOutOfBoundsException.class);
+		ArrayList<User> friendList = new ArrayList<>();
+		ArrayList<User> userList = new ArrayList<>();
+		userList.add(user1);
+		friendList.add(user1);
+		friendsService.removeFriendsfromUserList(userList, friendList);
+		friendList.get(-1);
+	}
+	
+	@Test
+	public void testForNullPointerExceptionRemoveFriendsFromUserList()
+	{
+		User user1 = factory.makeMockUserObject();
+		exception.expect(NullPointerException.class);
+		ArrayList<User> friendList = null;
+		ArrayList<User> userList = null;
+		friendsService.removeFriendsfromUserList(userList, friendList);
+		friendList.get(0);
+	}
+	
+	@After
+	public void tearDown() throws Exception 
+	{
+		factory = null;
+		friendsService = null;
+	}
 }
